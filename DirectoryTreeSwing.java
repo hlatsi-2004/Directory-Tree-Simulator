5ae1943c2d37253313af33aac3b687e6b2132a4d
Mahlatsi Suprise Mamorobela <160144006+hlatsi-2004@users.noreply.github.com>
2026-02-20 13:22:55 +0200

    Add files via upload

---
+++ DirectoryTreeSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.treesproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class TreesProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
